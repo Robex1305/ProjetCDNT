04defcd311e9e90135ba48e3eb2bb13e35819e22
Robex1305 <robex1305@gmail.com>
2019-08-30 01:40:37 +0200

    Pofinage événements

--- sport2go/src/app/tabEvenements/page-nouvel-evenement/page-nouvel-evenement.page.ts
+++ sport2go/src/app/tabEvenements/page-nouvel-evenement/page-nouvel-evenement.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Evenement } from 'src/models/classes/Evenement';
 import { NavigationExtras } from '@angular/router';
-import { NavController, ToastController, IonDatetime, Events } from '@ionic/angular';
+import { NavController, ToastController, IonDatetime, Events, IonTextarea } from '@ionic/angular';
 
 @Component({
   selector: 'app-page-nouvel-evenement',
@@ -12,31 +12,74 @@ import { NavController, ToastController, IonDatetime, Events } from '@ionic/angu
 export class PageNouvelEvenementPage implements OnInit {
   public evenement : Evenement;
   public dateEvenement : Date;
-  public heureEvenement : Date;
+
   toast : any;
 
   constructor(public navController : NavController, public events : Events, public toastController: ToastController) {
-    this.evenement = new Evenement();
+  }
+
+  get maxDate(){
+    let today = new Date();
+    let year = (today.getFullYear() + 2).toString();
+    return year;
+  }
+
+  get minDate(){
+    let today = new Date();
+    let year = today.getFullYear().toString();
+    return year;
   }
 
   ngOnInit() {
-    console.log("event init");
     this.evenement = new Evenement();
   }
 
   public createEvent(){
-    this.navController.pop();
-    this.evenement.setDateEvenement(this.dateEvenement,this.heureEvenement);
+    this.evenement.setDateEvenement(this.dateEvenement);
+    let check = this.validationEvenement();
+    if(check){
+      this.events.publish("nouvelEvenement:created",this.evenement);
+      this.navController.pop();
+    }
+  }
+
+  
+  public validationEvenement(){
+    let e = this.evenement;
+    let messageToast : string;
     
-    this.events.publish("nouvelEvenement:created",this.evenement);
+    let check = true;
+    if(e.titre === undefined || e.titre === ''){
+      messageToast = "Le titre ne peux pas être vide"
+      check = false
+    }
+    else if(e.titre.length < 5){
+      messageToast = "Le titre doit faire au moins 5 lettres"
+      check = false
+    }
+    else if(isNaN(e.dateEvenement.getTime())){
+      messageToast = "Veuillez saisir une date pour l'événement"
+      check = false
+    }
+    else if(e.geolocalisation.libelle === undefined || e.geolocalisation.libelle === ''){
+      messageToast = "Le lieu de l'événement n'est pas défini"
+      check = false
+    }else{
+      messageToast = "Événement créé avec succès";
+    }
+
     this.toast = this.toastController.create({
-      message: 'Evenement créé!',
+      message: messageToast,
       duration: 2000
     }).then((toastData)=>{
       console.log(toastData);
       toastData.present();
     });
-    
+
+    console.log(this.toast.message);
+    console.log(check)
+
+    return check;
   }
 
 }
