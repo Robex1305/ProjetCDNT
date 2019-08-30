cd9007ee4c1cb56d563daff3ab361a3bd5352be4
arobert <arobert@fichorga.fr>
2019-08-30 17:20:04 +0200

    Initialisation de la page profil + modifs mineurs

--- sport2go/src/app/app-routing.module.ts
+++ sport2go/src/app/app-routing.module.ts
@@ -7,11 +7,13 @@ const routes: Routes = [
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   { path: 'page-details-evenement', loadChildren: './pages/page-details-evenement/page-details-evenement.module#PageDetailsEvenementPageModule' },
-  { path: 'tabEvenements', loadChildren: './tabEvenements/tabEvenements.module#TabEvenementsPageModule' },
   { path: 'page-nouvel-evenement', loadChildren: './pages/page-nouvel-evenement/page-nouvel-evenement.module#PageNouvelEvenementPageModule' },
+  { path: 'tabEvenements', loadChildren: './tabEvenements/tabEvenements.module#TabEvenementsPageModule' },
   { path: 'page-logo', loadChildren: './pages/connexion/page-logo/page-logo.module#PageLogoPageModule' },
   { path: 'page-log-in', loadChildren: './pages/connexion/page-log-in/page-log-in.module#PageLogInPageModule' },
   { path: 'page-register', loadChildren: './pages/connexion/page-register/page-register.module#PageRegisterPageModule' },
+  { path: 'page-profil', loadChildren: './pages/page-profil/page-profil.module#PageProfilPageModule' },
+
 
 ];
 
