dac6834771d77079f8a73e9702aabbe427951968
seb <s.fellenberg@urbik.fr>
2019-09-02 00:45:16 +0200

    SEB - Ajout front de la page social

--- sport2go/src/app/tabs/tabs.router.module.ts
+++ sport2go/src/app/tabs/tabs.router.module.ts
@@ -38,12 +38,12 @@ const routes: Routes = [
         ]
       },
       {
-        path: 'tab2',
+        path: 'tabsocial',
         children: [
           {
             path: '',
             loadChildren: () =>
-              import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+              import('../tabsocial/tabsocial.module').then(m => m.TabsocialPageModule)
           }
         ]
       },
